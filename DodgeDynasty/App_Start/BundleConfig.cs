9929fac9f171d64259a17789c35a3daacac13123
gfh19 <gfh19@yahoo.com>
2015-03-28 17:08:21 -0400

    Initial bootstrap included (some css commented)

--- DodgeDynasty/App_Start/BundleConfig.cs
+++ DodgeDynasty/App_Start/BundleConfig.cs
@@ -8,24 +8,19 @@ namespace DodgeDynasty
 		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+			bundles.Add(new ScriptBundle("~/bundles/lib").Include(
 						"~/Scripts/lib/jquery-{version}.js",
 						"~/Scripts/lib/jquery-ui-{version}.js",
-						"~/Scripts/lib/jquery.cookie.js"));
-
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+						"~/Scripts/lib/jquery.cookie.js",
 						"~/Scripts/lib/jquery.unobtrusive*",
-						"~/Scripts/lib/jquery.validate*"));
-
-			// Use the development version of Modernizr to develop with and learn from. Then, when you're
-			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/lib/modernizr-*"));
+						"~/Scripts/lib/jquery.validate*",
+						"~/Scripts/lib/moment.min.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
-			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-						"~/Content/themes/base/jquery-ui-1.10.4.custom.css"));
+			bundles.Add(new StyleBundle("~/Content/css/lib").Include(
+						"~/Content/themes/base/jquery-ui-1.10.4.custom.css",
+						"~/Content/bootstrap/css/bootstrap*"));
 		}
 	}
 }
