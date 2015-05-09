d8ba670be4fc5dbec5d6100abc3835cbfc657810
gfh19 <gfh19@yahoo.com>
2015-05-09 01:49:09 -0400

    jQuery UI Dialog restored (pre-bootstrap)

--- DodgeDynasty/App_Start/BundleConfig.cs
+++ DodgeDynasty/App_Start/BundleConfig.cs
@@ -8,14 +8,17 @@ namespace DodgeDynasty
 		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles)
 		{
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+						"~/Scripts/lib/jquery-{version}.js",
+						"~/Scripts/lib/bootstrap/js/bootstrap.js")); //jquery MUST come before bootstrap, & before jquery-ui
+
 			bundles.Add(new ScriptBundle("~/bundles/lib").Include(
 						"~/Scripts/lib/jquery-{version}.js",
-						"~/Scripts/lib/jquery-ui-{version}.js",
 						"~/Scripts/lib/jquery.cookie.js",
 						"~/Scripts/lib/jquery.validate*",
 						"~/Scripts/lib/jquery.signalR-{version}.js",
 						"~/Scripts/lib/moment.js",
-						"~/Scripts/lib/bootstrap/js/bootstrap.js"));
+						"~/Scripts/lib/jquery-ui-{version}.js"));	//jquery-ui MUST come after bootstrap
 
 			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
