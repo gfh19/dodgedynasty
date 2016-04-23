096a51db394c304d766a73d7b8c8177b7651337f
gfh19 <gfh19@yahoo.com>
2016-04-23 00:23:09 -0400

    Player Adjustments - Initial mockups

--- DodgeDynasty/Models/PlayerAdjustments/PlayerAdjustmentsModel.cs
+++ DodgeDynasty/Models/PlayerAdjustments/PlayerAdjustmentsModel.cs
@@ -1,6 +1,11 @@
-﻿namespace DodgeDynasty.Models.PlayerAdjustments
+﻿using System.Collections.Generic;
+using DodgeDynasty.Models.Types;
+
+namespace DodgeDynasty.Models.PlayerAdjustments
 {
 	public class PlayerAdjustmentsModel
 	{
+		public List<AdjustedPlayer> AddedPlayers { get; set; }
+		public List<AdjustedPlayer> OtherAdjPlayers { get; set; }
 	}
 }
---
+++ DodgeDynasty/Models/Types/AdjustedPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DodgeDynasty.Models.Types
+{
+	public class AdjustedPlayer : RankedPlayer
+	{
+		public string Action { get; set; }
+		public string UserFullName { get; set; }
+		public string DraftsRanksText { get; set; }
+		public DateTime AddTimestamp { get; set; }
+	}
+}
