2923e821c120b0503a2e03f930d7f10167e54ca6
gfh19 <gfh19@yahoo.com>
2018-07-06 17:58:41 -0400

    SQL note updates

--- DodgeDynasty/2018 SQL Changes.sql
+++ DodgeDynasty/2018 SQL Changes.sql
@@ -26,8 +26,8 @@ SET ImportUrl = 'https://partners.fantasypros.com/api/v1/consensus-rankings.php?
 WHERE AutoImportId = 5
 
 
-/* Mark Todd Gurley II TPID (871) - Done */
-/* Consolidate Ronald Jones / II - Done */
+/* Mark Todd Gurley II TPID (871) */
+/* Consolidate Ronald Jones / II */
 /* Mark Marvin Jones Jr. 
 TPID (188) - Done */
 
@@ -36,218 +36,18 @@ TPID (188) - Done */
 Marquez Valdes-Scantling
 Damore'ea Stringfellow
 Tampa Bay Buccaneers
+Equanimeous St. Brown
 
 */
 
-
-
-
-COMMIT TRANSACTION;
-
-
-
-
-
-
-
-/* Below run in Production on 7/6/17 */
-
-SET XACT_ABORT ON
-BEGIN TRANSACTION;
-
-
-
-
-
-
-CREATE NONCLUSTERED INDEX [IX_RankId] ON [dbo].[PlayerRank]
-(
-	[RankId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-
-
-
-
-
-
-COMMIT TRANSACTION;
-
-
-
-
-
-
-
-/* Below run in Production on 6/10/17 */
-
-SET XACT_ABORT ON
-BEGIN TRANSACTION;
-
-
-
-
-
-ALTER TABLE [dbo].[DraftRank]  WITH CHECK ADD  CONSTRAINT [FK_DraftRank_Rank] FOREIGN KEY([RankId])
-REFERENCES [dbo].[Rank] ([RankId])
-GO
-
-ALTER TABLE [dbo].[DraftRank] CHECK CONSTRAINT [FK_DraftRank_Rank]
-GO
-
-
-DELETE
-  FROM [dbo].[DraftRank]
-  WHERE RankId = 68
-
-DELETE
-FROM [dbo].[Rank]
-  WHERE RankId = 68
-
-
-DELETE
-  FROM [dbo].[DraftRank]
-  WHERE RankId = 69
-
-DELETE
-FROM [dbo].[Rank]
-  WHERE RankId = 69
-
-
-
-
-COMMIT TRANSACTION;
-
-
-
-
-/* Below run in Production on 6/10/17 */
-
-SET XACT_ABORT ON
-BEGIN TRANSACTION;
-
-
-
-ALTER TABLE [dbo].[League]
-ADD [NumRounds] smallint NOT NULL DEFAULT(0);
-GO
-
-UPDATE [dbo].[League]
-SET NumRounds = 15
-WHERE LeagueId IN (1, 2, 7)
-
-UPDATE [dbo].[League]
-SET NumRounds = 17
-WHERE LeagueId IN (8)
-
-UPDATE [dbo].[League]
-SET NumRounds = 4
-WHERE LeagueId IN (3, 6, 9)
-
-UPDATE [dbo].[League]
-SET NumRounds = 6
-WHERE LeagueId IN (4)
-
-UPDATE [dbo].[League]
-SET NumRounds = 3
-WHERE LeagueId IN (5)
-
-UPDATE [dbo].[League]
-SET NumRounds = 8
-WHERE LeagueId IN (10)
-
-ALTER TABLE [dbo].[League]
-ADD [NumKeepers] smallint NOT NULL DEFAULT(0);
-GO
-
-UPDATE [dbo].[League]
-SET NumKeepers = 0
-
-UPDATE [dbo].[League]
-SET NumKeepers = 6
-WHERE LeagueId IN (1)
-
-UPDATE [dbo].[League]
-SET NumKeepers = 1
-WHERE LeagueId IN (2)
-
-ALTER TABLE [dbo].[League]
-ADD [Format] varchar(10) NOT NULL DEFAULT('snake')
-GO
-
-UPDATE [dbo].[League]
-SET [Format] = 'snake'
-
-UPDATE [dbo].[League]
-SET [Format] = 'repeat'
-WHERE LeagueId IN (1)
-
-ALTER TABLE [dbo].[League]
-ADD [CombineWRTE] bit NOT NULL DEFAULT(0);
-GO
-
-UPDATE [dbo].[League]
-SET [CombineWRTE] = 0
-
-UPDATE [dbo].[League]
-SET [CombineWRTE] = 1
-WHERE LeagueId IN (1, 2, 4, 8)
-
-ALTER TABLE [dbo].[League]
-ADD [PickTimeSeconds] smallint NOT NULL DEFAULT(120)
-GO
-
-
-
-ALTER TABLE [dbo].[Draft]
-ADD [CombineWRTE] bit NOT NULL DEFAULT(0);
-GO
-
-UPDATE [dbo].[Draft]
-SET [CombineWRTE] = 0
-
-UPDATE [dbo].[Draft]
-SET [CombineWRTE] = 1
-WHERE LeagueId IN (1, 2, 4, 8)
-
-ALTER TABLE [dbo].[Draft]
-DROP COLUMN [NumOwners];
-GO
-
-ALTER TABLE [dbo].[Draft]
-ALTER COLUMN [NumRounds] smallint NOT NULL;
-GO
-
-ALTER TABLE [dbo].[Draft]
-ALTER COLUMN [NumKeepers] smallint NOT NULL;
-GO
-
-ALTER TABLE [dbo].[Draft]
-ALTER COLUMN [Format] varchar(10) NOT NULL
-GO
-
-
-/* DON'T FORGET to set defaults for NumRounds, NumKeepers, Format, PickTimeSeconds, and CombineWRTE for all existing leagues !!! */
-
-
-ALTER TABLE [dbo].[PlayerRankOption]
-ADD [ExpandWR] bit NOT NULL DEFAULT(0)
-GO
-
-ALTER TABLE [dbo].[PlayerRankOption]
-ADD [HideWR] bit NOT NULL DEFAULT(0)
-GO
-
-ALTER TABLE [dbo].[PlayerRankOption]
-ADD [ExpandTE] bit NOT NULL DEFAULT(0)
-GO
-
-ALTER TABLE [dbo].[PlayerRankOption]
-ADD [HideTE] bit NOT NULL DEFAULT(0)
-GO
-
-
-
+/*
+  NEXT YEAR 2019:
+  Don't forget to re-order champion nameplate!
+  www.trophypartner.com
+  #NP100, Silver, 2" x 0.75"
+  (Or take trophy to local shop & ask to reproduce cheaper)
+
+*/
 
 
 COMMIT TRANSACTION;
